b1dfb7ce10e0029d40085a19b084ea88a34c2569
keyzen <alonso.anavalon@gmail.com>
2021-09-28 16:20:40 -0300

    first heroku commit

--- src/app.js
+++ src/app.js
@@ -2,8 +2,49 @@ const express = require('express')
 const app = express()
 
 
+const Handlebars = require('handlebars')
+const exphbs = require('express-handlebars')
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access');
+const path = require('path')
+const morgan = require('morgan')
+const cookieParser = require('cookie-parser')
+
+//Routes
+const indexRoutes = require('./routes/index')
+
 //SETTINGS
+
+const hbs = exphbs.create({
+    handlebars: allowInsecurePrototypeAccess(Handlebars), 
+    layoutsDir: path.join(__dirname, 'views', 'layouts'),
+    partialsDir: path.join(__dirname, 'views', 'partials'),
+    defaultLayout: 'main',
+    extname: '.hbs',
+
+    helpers: {
+        loud: function (aString) {
+            return aString.toUpperCase()
+        }
+}})
+
 app.set('port', process.env.PORT || 3000)
+app.set('views', path.join(__dirname, 'views'))
+app.engine('.hbs', hbs.engine)
+app.set('view engine', '.hbs')
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Middlewares
+
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
+
+//Routes
+
+app.use('/', indexRoutes)
+
+
 
 app.get('/', (req, res) => {
     res.send("okidoki")
