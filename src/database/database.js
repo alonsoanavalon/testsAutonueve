069f251d3f4826cacf6a52a4d3b4ac910f424ff7
keyzen <alonso.anavalon@gmail.com>
2021-09-28 16:52:56 -0300

    Fix

--- src/database/database.js
+++ src/database/database.js
@@ -2,6 +2,10 @@ const config = require('./cfg');
 const mysql = require('mysql');
 
 let pool = mysql.createPool({
+    connectionLimit : 1000,
+    connectTimeout  : 60 * 60 * 1000,
+    acquireTimeout  : 60 * 60 * 1000,
+    timeout         : 60 * 60 * 1000,
     host:config.host,
     user:config.user,
     password:config.password,
